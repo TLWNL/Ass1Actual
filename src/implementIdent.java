c771e08a826277481abe006680934f9905ccf4a6
TLWNL <dylanrijnsburger@gmail.com>
2018-09-16 15:45:20 +0200

    Added skeletor

---
+++ src/implementIdent.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class implementIdent {
+
+        // Initializes a StringBuffer
+        public static void initIdent(){
+
+        }
+
+        public static void firstCharCheck(Scanner in){
+            if(nextChar(in) != '{'){
+                //Throw an error
+            }
+
+        }
+        // Reads char for char using Scanner, until space has been read.
+        // Parses read data into a StringBuffer
+        public static void parseIdent(Scanner in, StringBuffer dest)
+        {
+            // Check if the first character that is to be read is a number
+            if(dest.length() == 0 && nextCharIsDigit(in)){
+                // Throw an error
+            }
+
+            while(!nextCharIs(in, ' ')){
+                dest.append(nextChar(in));
+            }
+        }
+
+        // Method to read 1 character
+        public static char nextChar(Scanner in){
+            return in.next().charAt(0);
+        }
+
+        // Method to check if the next character to be read when
+        // calling nextChar() is equal to the provided character.
+        public static boolean nextCharIs(Scanner in, char c){
+            return in.hasNext(Pattern.quote(c+""));
+        }
+
+        // Method to check if the next character to be read when
+        // calling nextChar() is a digit
+        public static boolean nextCharIsDigit(Scanner in){
+            return in.hasNext("[0-9]");
+        }
+
+}
---
+++ src/implementSet.java
@@ -0,0 +1,33 @@
+public class implementSet {
+
+    public static void initSet(){
+
+    }
+
+    // Takes a StringBuffer as input, and places it into the specified Array location
+    public static void parseSet(StringBuffer in, StringBuffer arrayDest){
+
+    }
+
+    // All elements contained in arrayInput1, but not in arrayInput2
+    public static void difference(StringBuffer arrayInput1, StringBuffer arrayInput2){
+
+    }
+
+    // All elements contained in both sets
+    public static void intersection(StringBuffer arrayInput1, StringBuffer arrayInput2){
+
+    }
+
+    // All elements of both sets
+    public static void union(StringBuffer arrayInput1, StringBuffer arrayInput2){
+
+    }
+
+    // All elements of both sets that are not contained in the intersection
+    public static void symmetricDifference(StringBuffer arrayInput1, StringBuffer arrayInput2){
+
+    }
+
+
+}
